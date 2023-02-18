291ca2336f4309d49844a1a1e5f57fd43024c512
benmandrew <benmandrew@gmail.com>
2023-02-18 23:11:40 +0100

    Valuation of sequents

--- vseq.v
+++ vseq.v
@@ -56,12 +56,53 @@ Inductive s_l : Set :=
   | s_l_nil : s_l
   | s_l_cons : prop -> s_l -> s_l.
 
+Fixpoint s_l_val (c : ctx) (s : s_l) {struct s} : option bool :=
+  match s with
+  | s_l_nil => Some true
+  | s_l_cons p s =>
+      match prop_val c p with
+      | None => None
+      | Some b1 =>
+          match s_l_val c s with
+          | None => None
+          | Some b2 => Some (andb b1 b2)
+          end
+      end
+  end.
+
 Inductive s_r : Set :=
   | s_r_nil : s_r
   | s_r_cons : prop -> s_r -> s_r.
 
+Fixpoint s_r_val (c : ctx) (s : s_r) {struct s} : option bool :=
+  match s with
+  | s_r_nil => Some false
+  | s_r_cons p s =>
+      match prop_val c p with
+      | None => None
+      | Some b1 =>
+          match s_r_val c s with
+          | None => None
+          | Some b2 => Some (orb b1 b2)
+          end
+      end
+  end.
+
 Inductive seq_t : Set :=
   | seq : s_l -> s_r -> seq_t.
+
+Definition seq_val (c : ctx) (s : seq_t) : option bool :=
+  match s with
+  | seq l r =>
+      match s_l_val c l with
+      | None => None
+      | Some b1 =>
+          match s_r_val c r with
+          | None => None
+          | Some b2 => Some (implb b1 b2)
+          end
+      end
+  end.
 
 Inductive eval : seq_t -> seq_t -> Prop :=
   | neg_l : forall p l r,
@@ -70,3 +111,9 @@ Inductive eval : seq_t -> seq_t -> Prop :=
   | neg_r : forall p l r,
       eval (seq l (s_r_cons (p_neg p) r))
            (seq (s_l_cons p l) r).
+
+Lemma eval_preserves_val : forall c s1 s2 b,
+  eval s1 s2 -> seq_val c s1 = Some b -> seq_val c s2 = Some b.
+Proof.
+  intros c s1 s2 b Hev Heq. induction Hev.
+    unfold seq_val in Heq. unfold s_l_val in Heq.
